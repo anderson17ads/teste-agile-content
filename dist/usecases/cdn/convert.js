a0711b52a1a1f53d6766d4eedc44f32d68217618
Anderson de Souza <anderson17ads@hotmail.com>
2022-03-13 01:55:50 -0300

    Ignoring files of dist path

--- dist/usecases/cdn/convert.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Convert = void 0;
-class Convert {
-    execute() {
-        console.log('Convert');
-    }
-}
-exports.Convert = Convert;
--- dist/usecases/cdn/read-source-url.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReadSourceUrl = void 0;
-class ReadSourceUrl {
-    execute(sourceUrl) {
-        return sourceUrl;
-    }
-}
-exports.ReadSourceUrl = ReadSourceUrl;
